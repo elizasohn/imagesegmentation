cea9bbc627ee688a8e9e925bc5238da0eb5cb4d6
Eliza Sohn <eliza.sohn@gmail.com>
2023-06-08 00:31:37 -0700

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -1,14 +1,24 @@
+const observer = (
+  targetNode,
+  callback,
+  config = { childList: true, subtree: true, attributes: true }
+) => {
+  const obs = new MutationObserver(callback);
+  obs.observe(targetNode, config);
+	return observer;
+};
+
 const draw = () => {
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
   const img = new Image(100, 100);
-  img.src = 'https://www.tripsavvy.com/thmb/c5nIYqYcJf_hvTS1Qu4th0Mea5A=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/CratermainGettyImages-1158150928-0097b7d1b250427f96e90868a3cb3cf8.jpg';
+  img.src = '../images/craterlake.jpeg';
   img.crossOrigin = 'Anonymous';
   let imgData;
 
   const drawImage = (img) => {
-    canvas.width = img.naturalWidth/4;
-    canvas.height = img.naturalHeight/4;
+    canvas.width = img.naturalWidth/1.5;
+    canvas.height = img.naturalHeight/1.5;
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
   }
 
@@ -19,17 +29,16 @@ const draw = () => {
   }
 
   const selectInitialCentroids = (imgRGBData, k) => {
-    // const points = (imgRGBData.width * 3) + (imgRGBData.height * 3);
     const centroidsIndex = [];
     let index;
-    // add check that index isn't already in centroidsIndex array
     for (let i = 0; i < k; i += 1) {
-      let index = Math.floor(Math.random() * imgRGBData.length);
+      index = Math.floor(Math.random() * imgRGBData.length);
+      // check if index is already in centroidsIndex
+      while (centroidsIndex.includes(index)) {
+        index = Math.floor(Math.random() * imgRGBData.length);
+      }
       centroidsIndex.push(index);
     };
-
-    console.log('centroids index', centroidsIndex);
-
     const centroids = [];
     for (let i = 0; i < centroidsIndex.length; i += 1) {
       const centroid = imgRGBData[centroidsIndex[i]];
@@ -38,22 +47,173 @@ const draw = () => {
     return centroids;
   };
 
+  const initializeCentroidsNaiveSharding = (imgRGBData, k) => {
+    const centroidsIndex = [];
+    let index;
+  }
+
   const imgRGBData = [];
 
   const createRGBPixelData = () => {
     console.log('img data length', imgData.data.length);
     for (let i = 0; i < imgData.data.length; i += 4) {
-      console.log(i);
       const r = imgData.data[i];
       const g = imgData.data[i + 1];
       const b = imgData.data[i + 2];
       const pixel = [r, g, b];
-      console.log('pixel', pixel)
       imgRGBData.push(pixel);
     }
-    // console.log('img rgb data', imgRGBData);
-    // return imgRGBData;
+  };
+
+  const createCanvasData = (newRGBData, width, height) => {
+    const canvasData = [];
+    console.log('test rgb', newRGBData[500]);
+    for (let i = 0; i < newRGBData.length; i += 1) {
+      for (let j = 0; j < 4; j += 1) {
+        if (j < 3) {
+          canvasData.push(newRGBData[i][j]);
+        } else {
+          canvasData.push(255);
+        }
+      }
+    }
+    const uint8 = new Uint8ClampedArray(canvasData);
+    console.log('canvas data: ', canvasData);
+    const newImageData = new ImageData(new Uint8ClampedArray(canvasData), width, height);
+    return newImageData;
+  };
+
+  const assignToCentroids = (imgRGBData, centroids) => {
+    const assignments = {};
+    for (let i = 0; i < centroids.length; i += 1) {
+      assignments[i] = {
+        pixels: [],
+        centroid: centroids[i],
+      };
+    };
+    for (const element of imgRGBData) {
+      const pixel = element;
+      let closestCentroid, closestCentroidIndex, previousDistance;
+      for (let i = 0; i < centroids.length; i += 1) {
+        const centroid = centroids[i];
+        if (i === 0) {
+          closestCentroid = centroid;
+          closestCentroidIndex = i;
+          previousDistance = getDistance(pixel, closestCentroid);
+        } else {
+          const distance = getDistance(pixel, centroid);
+          if (distance < previousDistance) {
+            previousDistance = distance;
+            closestCentroid = centroid;
+            closestCentroidIndex = i;
+          }
+        }
+      }
+      assignments[closestCentroidIndex].pixels.push(imgRGBData.indexOf(pixel));
+    }
+    return assignments;
   }
+
+  const getMeans = (pixels) => {
+    const totalPixels = pixels.length;
+    const means = [];
+    for (let i = 0; i < 3; i += 1) {
+      means.push(0);
+    }
+    for (const element of pixels) {
+      const pixel = imgRGBData[element];
+      for (let j = 0; j < pixel.length; j += 1) {
+        const value = pixel[j];
+        means[j] += value;
+      }
+    }
+    for (let i = 0; i < 3; i += 1) {
+      means[i] = Math.round(means[i] / totalPixels);
+    }
+    return means;
+  };
+
+  const recalculateCentroids = (imgRGBData, assignments, k) => {
+    let newCentroid;
+    const newCentroidList = [];
+    for (const k in assignments) {
+      const centroidGroup = assignments[k];
+      if (centroidGroup.pixels.length > 0) {
+        newCentroid = getMeans(centroidGroup.pixels);
+      } else {
+        newCentroid = selectInitialCentroids(imgRGBData, 1)[0];
+      }
+      newCentroidList.push(newCentroid);
+    }
+    return newCentroidList;
+  }
+
+  const updateImgData = (assignments, imgRGBData, k) => {
+    const newRGBData = [];
+    for (const element of imgRGBData) {
+      newRGBData.push(0);
+    };
+    for (let i = 0; i < k; i += 1) {
+      let centroidRGBData = assignments[i].centroid;
+      console.log('assignments centroid: ', centroidRGBData);
+      assignments[i].pixels.forEach((pixel) => {
+        newRGBData[pixel] = centroidRGBData;
+      })
+    };
+    return newRGBData;
+  };
+
+  const runKmeans = (imgRGBData, k, maxIterations, width, height) => {
+    let iterations = 0;
+    let oldCentroids, assignments, centroids;
+
+    //initialize centroids
+    centroids = selectInitialCentroids(imgRGBData, k);
+
+    // run algorithm
+    while (iterations < maxIterations) {
+      console.log('iteration: ', iterations);
+      oldCentroids = [...centroids];
+      iterations += 1;
+
+      assignments = assignToCentroids(imgRGBData, centroids);
+      centroids = recalculateCentroids(imgRGBData, assignments, k);
+    }
+    // const clusters = [];
+    // for (let i = 0; i < k; i += 1) {
+    //   clusters.push(assignments[i]);
+    // }
+
+    // const results = {
+    //   clusters: clusters,
+    //   centroids: centroids,
+    //   iterations: iterations,
+    // };
+
+    // console.log('assignments', assignments);
+    const newRGBData = updateImgData(assignments, imgRGBData, k);
+    // console.log('new rgb data', newRGBData);
+    const canvasData = createCanvasData(newRGBData, canvas.width, canvas.height);
+    return canvasData;
+  }
+  
+  const showLoading = () => {
+    document.querySelector('.submit').classList.add('hidden');
+    document.querySelector('.loading').classList.remove('hidden');
+  };
+
+  const hideLoading = () => {
+    document.querySelector('.submit').classList.remove('hidden');
+    document.querySelector('.loading').classList.add('hidden');
+  };
+
+  const displayResults = (k, iterations) => {
+    const canvasData = runKmeans(imgRGBData, k, iterations, canvas.width, canvas.height);
+    console.log('canvas data after kmeans: ', canvasData);
+    ctx.putImageData(canvasData, 0, 0);
+    hideLoading();
+  };
+
 
   img.onload = () => {
     drawImage(img);
@@ -61,39 +221,26 @@ const draw = () => {
     console.log('image data', imgData);
     createRGBPixelData();
     console.log('rgb data', imgRGBData);
+    const submitBtn = document.querySelector('.submit');
+    const submitDiv = document.querySelector('.submit-container');
+    const loading = document.querySelector('.loading');
+    const kslider = document.getElementById('k-fader');
+    const islider = document.getElementById('i-fader');
 
-    const centroids = selectInitialCentroids(imgRGBData, 2);
-    console.log('centroids', centroids);
-    const start = imgRGBData[0];
-    centroids.forEach((centroid) => {
-      const distance = getDistance(centroid, start);
-      console.log('distance', distance);
+    observer(loading, (mutationList) => {
+      if (!mutationList[0].target.classList.contains('hidden')) {
+        setTimeout(() => {
+          displayResults(kslider.value, islider.value);
+        }, 500);
+      }
     })
-
-    // console.log('pixel length', imgRGBData.length);
-    // console.log('sample pixel: :', imgRGBData[34444]);
-    // convertRGBToHex(imgData.data);
-    // console.log('hex values', hexValues);
+    
+    submitDiv.addEventListener('click', () => {
+      showLoading();
+    })
+    
+    // const canvasData = runKmeans(imgRGBData, 5, 2, canvas.width, canvas.height);
+    // console.log('canvas data after kmeans: ', canvasData);
+    // ctx.putImageData(canvasData, 0, 0);
   };
-
-    // const hexValues = [];
-
-  // const valueToHex = (color) => {
-  //   console.log('color, ', color);
-  //   const hex = color.toString(16);
-  //   return hex;
-  // }
-  // const convertRGBToHex = () => {
-  //   console.log('img data length', imgData.data.length);
-  //   for (let i = 0; i < imgData.data.length; i += 4) {
-  //     console.log(i);
-  //     const r = imgData.data[i];
-  //     const g = imgData.data[i + 1];
-  //     const b = imgData.data[i + 2];
-  //     // const a = imgData.data[i + 3];
-  //     const hexValue = '#' + valueToHex(r) + valueToHex(g) + valueToHex(b);
-  //     hexValues.push(hexValue);
-  //   }
-  // }
-
 };
