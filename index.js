8ce9516222e3a65e122da5c42250c426feaad42a
Eliza Sohn <eliza.sohn@gmail.com>
2023-06-08 00:31:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const http = require('http');
-// create the server object
-const server = http.createServer((req, res) => {
-res.writeHead(200, { 'Content-Type': 'text/plain' }) // response headers
-res.write('Hello World, Node!'); // write a response to the client
-res.end(); // end the response
-});
-// set the server to listen on port 5000
-server.listen(8000, () => {
-console.log(`Server running at http://localhost:8000`);
-});
+// const http = require('http');
+// const fs = require('fs');
+// // create the server object
+// const server = http.createServer((req, res) => {
+// res.writeHead(200, { 'Content-Type': 'text/html' }) // response headers
+//   // res.render('index.html'); // write a response to the client
+//   fs.createReadStream('index.html').pipe(res)
+// // res.end(); // end the response
+// });
+// // set the server to listen on port 5000
+// // server.listen(8000, () => {
+// //   console.log(`Server running at http://localhost:8000`);
+//   server.listen(process.env.PORT || 8000);
+// // });
